a981bb4131bb1b6c10359edc06a400295937b6af
Burcin Erocal <burcin@erocal.org>
2013-04-06 16:50:31 +0200

    Add example 2 from Rubner, Tomasi, Guibas code as test case for EMD.
    Two examples are providd in [1]. This patch adds the test case in
    example2.c from [1] as a unittest for Earth Movers Distance (EMD) function
    using the original code from [1].
    
    [1] http://robotics.stanford.edu/~rubner/emd/default.htm
    
    Since C++ does not allow nested function definitions and static members for
    local classes, the distance function is added as a static method in the
    EarthMoverDistanceTest class. Eventually the distance method should be
    moved to a functor.

--- test/emd/test.cxx
+++ test/emd/test.cxx
@@ -35,6 +35,9 @@
 
 #include <unittest.hxx>
 #include <vigra/emd.hxx>
+extern "C" {
+#include "emd.h"
+}
 
 using namespace vigra;
 
@@ -45,11 +48,141 @@ public:
     EarthMoverDistanceTest()
     {}
 
-        // add your test functions here
-    void testEMD()
+    typedef float ValueType;
+
+    // Check that the flow given by Flow between signatures Signature1 and
+    // Signature2 satisfies some basic properties.
+    //
+    // Let Signature1 and Signature2 have labels l_i and l_j with
+    // corresponding weights w_i and w_j respectively.
+    //
+    // Consider the flow as a list of arrows each with origin i, target
+    // j and weight f_ij. Then
+    //
+    // 1) f_ij >= 0 for all i, j
+    //      the flow is only in one direction
+    // 2) \sum_i f_ij <= w_i
+    //      sum of arrows originating in the i-th coordinate is <= w_i
+    // 3) \sum_j f_ij <= w_j
+    //      sum of arrows ending in the j-th coordinate is <= w_j
+    // 4) \sum_{i,j} f_ij = min(\sum_i w_i, \sum_j w_j)
+    //      sum of the total flow does not exceed total available earth or
+    //      space
+    //
+    // See page 8 of the following text for further explanation:
+    //
+    // http://www.cs.cmu.edu/~efros/courses/AP06/Papers/rubner-jcviu-00.pdf
+    void checkFlowProperties(signature_t *Signature1,
+            signature_t *Signature2, flow_t *Flow, int FlowSize)
     {
-        
+        ValueType source_total = 0;
+        ValueType dest_total = 0;
+        ValueType total_flow = 0;
+        // check for positive flow, compute total flow and make sure the
+        // origin and destination labels on the flow correspond to valid
+        // labels of the source and target signatures
+        for (int i=0; i < FlowSize; ++i)
+        {
+            shouldMsg(Flow[i].amount >= 0, "flow in the wrong direction");
+            total_flow += Flow[i].amount;
+
+            shouldMsg(Flow[i].from >=0 && Flow[i].from < Signature1->n,
+                    "Flow origin label not found in Signature");
+            shouldMsg(Flow[i].to >=0 && Flow[i].to < Signature2->n,
+                    "Flow destination label not found in Signature");
+        }
+
+        // compute total initial and target weights
+        for (int i=0; i < Signature1->n; ++i)
+        {
+            source_total += Signature1->Weights[i];
+        }
+        for (int i=0; i < Signature2->n; ++i)
+        {
+            dest_total += Signature2->Weights[i];
+        }
+        shouldMsg(total_flow == std::min(source_total, dest_total),
+                "total flow more than minimum of total initial or target");
+
+        // check if source bins are used within their limits
+        for (int i=0; i < Signature1->n; ++i)
+        {
+            ValueType tot = 0;
+            for (int j=0; j < FlowSize; ++j)
+            {
+                if(Flow[j].from == i)
+                    tot += Flow[j].amount;
+            }
+            shouldMsg(tot <= Signature1->Weights[i],
+                    "flow uses more earth than available in bin");
+        }
+
+        // check if target bins are not filled over their capacity
+        for (int i=0; i < Signature2->n; ++i)
+        {
+            ValueType tot = 0;
+            for (int j=0; j < FlowSize; ++j)
+            {
+                if(Flow[j].to == i)
+                    tot += Flow[j].amount;
+            }
+            shouldMsg(tot <= Signature2->Weights[i],
+                    "overflow in target bin");
+        }
     }
+
+    // temporary workaround for a distance function that works with the
+    // definition in the original emd sources.
+    // Should be replaced by a functor.
+    static ValueType dist_example2(int* F1, int* F2)
+    {
+        // Cost matrix as defined in example2 of original sources
+        // http://robotics.stanford.edu/~rubner/emd/default.htm
+        static ValueType _COST[5][3] = {
+            {3, 5, 2},
+            {0, 2, 5},
+            {1, 1, 3},
+            {8, 4, 3},
+            {7, 6, 5}
+            };
+        return _COST[*F1][*F2];
+    }
+
+    // example 2 on
+    // http://robotics.stanford.edu/~rubner/emd/default.htm
+    void testEMD_RTG_example2()
+    {
+        feature_t   f1[5] = { 0, 1, 2, 3, 4 },
+                    f2[3] = { 0, 1, 2 };
+        ValueType   w1[5] = { 0.4, 0.2, 0.2, 0.1, 0.1 },
+                    w2[3] = { 0.6, 0.2, 0.1 };
+        signature_t s1 = { 5, f1, w1},
+                    s2 = { 3, f2, w2};
+
+        ValueType   e;
+        flow_t      flow[7];
+        int         i, flowSize;
+
+        /* original source returns
+         *
+         * emd=1.888889
+         *
+         * flow:
+         * from to  amount
+         * 1    0   0.200000
+         * 0    0   0.300000
+         * 2    0   0.100000
+         * 3    1   0.100000
+         * 2    1   0.100000
+         * 4    2   0.000000
+         * 0    2   0.100000
+         */
+        e = emd(&s1, &s2, dist_example2, flow, &flowSize);
+        shouldEqualMessage(flowSize, 7, "flow should have 7 items");
+        shouldEqualToleranceMessage(e, 1.888889, 1e-6, "Earth Moving Distance not withing tolerance");
+        checkFlowProperties(&s1, &s2, flow, flowSize);
+    }
+
 };
 
 
@@ -59,7 +192,7 @@ struct HistogramDistanceTestSuite : public vigra::test_suite
     HistogramDistanceTestSuite()
         : vigra::test_suite("HistogramDistanceTestSuite")
     {
-        add(testCase(&EarthMoverDistanceTest::testEMD));
+        add(testCase(&EarthMoverDistanceTest::testEMD_RTG_example2));
     }
 };
 
